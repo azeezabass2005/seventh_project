0c4d18df3b0698d3afc824c6f019bf4cf2c03c85
Codegem <117828916+azeezabass2005@users.noreply.github.com>
2023-08-16 21:57:20 +0100

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 export const styles = {
     flexCenter:'flex justify-center items-center',
-    heading2: 'font-poppins font-semibold md:text-[50px] text-[40px] md:leading-[60px] leading-[50px] text-white',
+    heading2: 'font-poppins font-semibold md:text-[50px] text-[35px] md:leading-[60px] leading-[50px] text-white',
     paragraph: 'font-poppins text-dimWhite md:text-[22px] text-[18px] leading-[40px',
     sectionHeader: 'text-white bg-shbg font-poppins text-[38px] md:w-[90%] w-[85%] flex p-5 sm:m-15 m-7 text-white justify-center items-center rounded-[20px]'
 }
