155588bb8609dfc9921250627bd30de5c73486fa
Codegem <117828916+azeezabass2005@users.noreply.github.com>
2024-08-06 09:16:46 +0100

    fix: changed the font-size of the hero text

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactMe = () => {
   return (
     <a href="#contact">
-          <button className={`text-white font-poppins bg-gradient sm:px-5 sm:py-5 px-3 py-3 sm:text-[32px] text-[28px] sm:w-[240px] w-[220px] rounded-[10px] sm:mt-10 mt-5`}>Hire me</button>
+          <button className={`text-white font-poppins bg-gradient sm:px-5 sm:py-4 px-3 py-3 sm:text-[30px] text-[28px] sm:w-[220px] w-[220px] rounded-[10px] sm:mt-10 mt-5`}>Hire me</button>
     </a>
   )
 }
