6714f6b5f44707ed724518e2ddd1cda93dad50c0
Codegem <117828916+azeezabass2005@users.noreply.github.com>
2024-08-06 09:20:22 +0000

    chore: restored to normal

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,9 @@ function App() {
       <div className={`sm:py-5 sm:px-10 py-1 px-2 w-full top-0 fixed fixed-nav z-[10]`}>
         <Navbar />
       </div>
-      <div>
-        <div className="relative h-full w-full bg-[#080A19]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div></div>
         <div className={`bg-primary sm:pt-[80px] pt-[55px]`}>
           <Hero />
         </div>
-      </div>
       <div className={`bg-primary`}>
         <About />
         <Skills />
