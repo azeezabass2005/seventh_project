13b737136934bded0d9c76f6343ef2aa07e7c2c5
Azeez Abass Afolarin <117828916+azeezabass2005@users.noreply.github.com>
2024-09-04 16:53:30 +0000

    fix: mobile view fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
       <motion.div transition={{ duration: 0.5, delay: 0.1 }} whileInView={{ x:[150, 0], opacity:[0, 1] }}  className='relative'>
         <div className={`sm:h-[600px] sm:w-[600px] w-[360px] h-[360px] object-contain relative z-[1] mt-3 flex justify-center items-center`}>
         <img  src={profilepic} alt="my image" className='w-[300px] h-[300px]' />
-        <div className={`hero-skill-ring sm:w-[450px] sm:h-[450px] w-[280px] h-[280px] rounded-full absolute xs:left-[75px] xs:top-[75px] left-[80px] top-[80px]`}>
+        <div className={`hero-skill-ring sm:w-[450px] sm:h-[450px] w-[320px] h-[320px] rounded-full absolute xs:left-[75px] xs:top-[75px] left-[20px] top-[20px]`}>
           <img src={reactjs} alt="reactjs" className={`absolute w-[50px] h-[50px] object-contain sm:top-[10%] top-[8%] sm:left-[8%] left-[6%]`} />
           <img src={javascript} alt="javascript" className={`absolute w-[50px] h-[50px] object-contain sm:right-[8%] right-[6%] sm:top-[10%] top-[8%]`} />
           <img src={html} alt="html" className={`absolute w-[50px] h-[50px] object-contain sm:bottom-[10%] bottom-[8%] sm:left-[8%] left-[6%]`} />
